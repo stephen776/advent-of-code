22366d958faa94128006ef2acb8b7f0029d3780a
Steve Jacobs <sjacobs@truefit.io>
2022-12-01 06:27:26 -0500

    initial commit - first advent of code challenge 2022

---
+++ 2022/1/a.ts
@@ -0,0 +1,24 @@
+const input = await Deno.readTextFile('./input.txt');
+
+// split by the new line to get the groups
+const groups = input.split(/\ns*\n/);
+
+// get sum for each group
+const sums = groups.map((g) => {
+  const a = g.split('\n').filter((x) => x !== '');
+
+  const total = a.reduce((acc, curr) => {
+    return acc + parseInt(curr);
+  }, 0);
+
+  return total;
+});
+
+// sort the sums to get the top;
+const sorted = sums.sort((a, b) => b - a);
+
+// part 2 - sum the top 3
+const sliced = sorted.slice(0, 3);
+const total = sliced.reduce((acc, curr) => acc + curr, 0);
+
+console.log(total);
